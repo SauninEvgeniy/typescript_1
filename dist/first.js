a7dd25703e8153c5184465b090327c0bb96b75ab
Evgeniy Saunin <kromyx@yandex.ru>
2025-02-19 17:18:15 +0300

    add tsconfig

--- dist/first.js
+++ dist/first.js
@@ -1,3 +1,6 @@
-var alert2;
-alert2 = 'ALERT';
-console.log(alert2);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.alert3 = exports.alert2 = void 0;
+exports.alert2 = '123';
+exports.alert3 = '123';
+console.log(exports.alert2 + exports.alert3);
--- src/first.ts
+++ src/first.ts
@@ -1,3 +1,4 @@
-let alert2: string
+export let alert2: string
 alert2 = '123'
-console.log(alert2)
+export let alert3: string = '123'
+console.log(alert2 + alert3)
